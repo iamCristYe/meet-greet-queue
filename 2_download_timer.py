2f3331a109d6dde2c858baa9225c3c9a9ccdad1e
CY <iamCristYE@Gmail.com>
2024-09-15 19:37:47 +0800

    20240915

--- 2_download_timer.py
+++ 2_download_timer.py
@@ -2,34 +2,47 @@ import subprocess
 import time
 from datetime import datetime
 
+
 def save_curl_result():
     # 定义curl命令
     curl_command = [
-        'curl', 
-        '-d', '{"eventId":"e24082"}', 
-        '-H', 'Content-Type: application/json', 
-        '-X', 'POST', 'https://meets.fortunemusic.app/lapi/v5/app/dateTimezoneMessages'
+        "curl",
+        "-d",
+        '{"eventId":"e24409"}',
+        "-H",
+        "Content-Type: application/json",
+        "-X",
+        "POST",
+        "https://meets.fortunemusic.app/lapi/v5/app/dateTimezoneMessages",
     ]
-    
+
     # 获取当前时间戳
-    timestamp = datetime.now().strftime('%m%d-%H%M%S')
-    
+    timestamp = datetime.now().strftime("%m%d-%H%M%S")
+
     # 定义输出文件名
-    output_file = f'{timestamp}.json'
-    
+    output_file = f"{timestamp}.json"
+
     # 执行curl命令并将结果保存到文件
-    with open(output_file, 'w') as file:
+    with open(output_file, "w") as file:
         result = subprocess.run(curl_command, stdout=file)
-    
+
     if result.returncode == 0:
         print(f"Successfully saved curl result to {output_file}")
     else:
         print(f"Failed to execute curl command, return code: {result.returncode}")
 
+
 print("Script started. Press Ctrl+C to exit.")
 try:
     while True:
-        save_curl_result()
-        time.sleep(10)  # 等待10秒
+
+        current_time = datetime.now()
+        print(current_time)
+        if (
+            current_time.hour >= 8 and current_time.hour <= 21
+        ):  # current_time.minute >= 30:
+            save_curl_result()
+            # time.sleep(10)  # 等待10秒
+        time.sleep(20)
 except KeyboardInterrupt:
     print("Script stopped.")
